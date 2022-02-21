9439cd34488207621ade05ec730e4aab5dff20d3
Ferhat KAYMAZ <ferhatkaymaz2023@gmail.com>
2022-02-21 20:04:36 +0300

    started proje

---
+++ js/app.js
@@ -0,0 +1,13 @@
+    const form = document.getElementById('form');
+    const username = document.getElementById('username');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const repasssword = document.getElementById('repassword');
+
+
+    form.addEventListener('submit',(event) => {
+
+        
+
+        event.preventDefault();
+    });
